fe2928333c6d66136868acd9e0f0c27686e043f5
ArcPR <arcprax@gmail.com>
2023-03-25 19:54:24 -0400

    commit

--- StateMachine/Player_Container.gd
+++ StateMachine/Player_Container.gd
@@ -1,11 +1,13 @@
 extends Node2D
 
 onready var Player = load("res://Player/Player.tscn")
-var starting_position = Vector2(200,200)
+var starting_position = Vector2(100,300)
 
 
 func _physics_process(_delta):
-	if not has_node("Player"):
-		var player = Player.instance()
+	var player = get_node_or_null("Player")
+	if player == null:
+		player = Player.instance()
 		player.position = starting_position
+		player.name = "Player"
 		add_child(player)
